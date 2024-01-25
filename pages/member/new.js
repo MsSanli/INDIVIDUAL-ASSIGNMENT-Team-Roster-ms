179250e1466c9f36e92f01c3e9c9a73d368658f3
Maria Sanli <marisanli.93@gmail.com>
2024-01-24 21:24:38 -0500

    new form routing working

---
+++ pages/member/new.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import MemberForm from '../../components/forms/memberForm';
+
+export default function AddNewMember() {
+  return <MemberForm />;
+}
---
+++ pages/members.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { getMembers } from '../api/memberData';
+import { useAuth } from '../utils/context/authContext';
+import MembCard from '../components/MembCard';
+
+function CatMembers() {
+  // TODO: Set a state for books
+  const [members, setMembers] = useState([]);
+
+  // TODO: Get user ID using useAuth Hook
+  const { user } = useAuth();
+
+  // TODO: create a function that makes the API call to get all the books
+  const getAllTheMembers = () => {
+    getMembers(user.uid).then(setMembers);
+  };
+
+  // TODO: make the call to the API to get all the books on component render
+  useEffect(() => {
+    getAllTheMembers();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/member/new" passHref>
+        <Button>Add New Member</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {/* TODO: map over books here using BookCard component */}
+        {members.map((member) => (
+          <MembCard key={member.firebaseKey} memberObj={member} onUpdate={getAllTheMembers} />
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default CatMembers;
