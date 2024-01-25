66ce45c4946b3c68d8f002f59747743fc5e3840b
Maria Sanli <marisanli.93@gmail.com>
2024-01-24 20:33:15 -0500

    added some components minus nav

---
+++ components/MembCard.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteSingleMember } from '../api/memberData';
+
+function MembCard({ memberObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE AUTHOR AND HAVE THE VIEW RERENDER,
+  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE MEMBER
+  const deleteThisMemb = () => {
+    if (window.confirm(`Delete ${memberObj.name}?`)) {
+      deleteSingleMember(memberObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={memberObj.image} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{memberObj.name}</Card.Title>
+        {/* author email? */}
+        <Link href={`/members/${memberObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        <Link href={`/members/edit/${memberObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisMemb} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+MembCard.propTypes = {
+  memberObj: PropTypes.shape({
+    image: PropTypes.string,
+    name: PropTypes.string,
+    role: PropTypes.string,
+    firebaseKey: PropTypes.string,
+    uid: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default MembCard;
---
+++ components/forms/memberForm.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import PropTypes from 'prop-types';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../../utils/context/authContext';
+import { createMember, updateMember } from '../../api/memberData';
+
+const initialState = {
+  image: '',
+  name: '',
+  role: '',
+};
+
+function MemberForm({ obj }) {
+  const [formInput, setFormInput] = useState(initialState);
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (obj.firebaseKey) setFormInput(obj);
+  }, [obj, user]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (obj.firebaseKey) {
+      updateMember(formInput).then(() => router.push(`/members/${obj.firebaseKey}`));
+    } else {
+      const payload = { ...formInput, uid: user.uid };
+      createMember(payload).then(({ name }) => {
+        const patchPayload = { firebaseKey: name };
+        updateMember(patchPayload).then(() => {
+          router.push('/');
+        });
+      });
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2 className="text-white mt-5">{obj.firebaseKey ? 'Update' : 'Create'} MEMBER</h2>
+
+      {/* name INPUT  */}
+      <FloatingLabel controlId="floatingInput1" label="Name" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Enter Kitty Name"
+          name="cat_name"
+          value={formInput.cat_name}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* IMAGE INPUT  */}
+      <FloatingLabel controlId="floatingInput2" label="Member Image" className="mb-3">
+        <Form.Control
+          type="url"
+          placeholder="Upload image of 97 alter egos"
+          name="image"
+          value={formInput.image}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* EMAIL INPUT  */}
+      <FloatingLabel controlId="floatingInput3" label="role" className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Brooklyn Hipster"
+          name="role"
+          value={formInput.role}
+          onChange={handleChange}
+          required
+        />
+      </FloatingLabel>
+
+      {/* SUBMIT BUTTON  */}
+      <Button type="submit">{obj.firebaseKey ? 'Update' : 'Create'} Add Cat</Button>
+    </Form>
+  );
+}
+
+MemberForm.propTypes = {
+  obj: PropTypes.shape({
+    // eslint-disable-next-line react/no-typos
+    name: PropTypes.cat_name,
+    image: PropTypes.string,
+    role: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }),
+};
+
+MemberForm.defaultProps = {
+  obj: initialState,
+};
+
+export default MemberForm;
